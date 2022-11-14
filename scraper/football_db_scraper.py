bd2ebd7c1a3b2ebb1b6ff450e0b3e565445653a3
Jackson Crawford <jacksoncrawford@protonmail.com>
2022-11-14 12:56:31 -0800

    update scraper structure and add more recent data

---
+++ scraper/football_db_scraper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+from scraper import Scraper
+from html_cleaner import HTML_Cleaner
+
+class fdb_scraper():
+    scraper = None
+    cleaner = None
+
+    def __init__(self):
+        self.scraper = Scraper()
+        self.cleaner = HTML_Cleaner()
+
+    def wipe_file(self, file):
+        with open(file, 'w') as file:
+            pass
+
+    def scrape(self, url, output_file):
+        # Store soup (undecided on type, leaning towards chicken noodle)
+        soup = self.scraper.make_soup(url)
+        clean_soup = self.scraper.strain(soup, "statistics scrollable")
+
+        self.wipe_file(output_file)
+
+        rows = self.cleaner.split_on_tag(soup, "tr")
+        with open(output_file, 'a') as outfile:
+            writer = csv.writer(outfile)
+            for row in rows:
+                row = row[1]
+                row = self.cleaner.replace_tag(row, "</td>", ',')
+                row = self.cleaner.replace_tag(row, "</th>", ',')
+                row = self.cleaner.remove(row, "<span class=\"visible-xs\">", "</span>")
+                row = self.cleaner.remove_tag(row, 'a')
+                row = self.cleaner.remove_tag(row, "span")
+                row = self.cleaner.remove_tag(row, "td")
+                row = self.cleaner.remove_tag(row, "th")
+                row = row[:-1]
+
+                writer.writerow(row.split(","))
--- scraper/pass_scraper.py
+++ scraper/pass_scraper.py
@@ -4,36 +4,11 @@ import re
 import csv
 from scraper import Scraper
 from html_cleaner import HTML_Cleaner
+from football_db_scraper import fdb_scraper
 
 def main():
-    url="https://www.footballdb.com/statistics/nfl/player-stats/passing/2022/regular-season?sort=passyds"
-    # Store soup (undecided on type, leaning towards chicken noodle)
-    scraper = Scraper()
-    soup = scraper.make_soup(url)
-    clean_soup = scraper.strain(soup, "statistics scrollable")
-
-    output_file = "../data/rushing.csv"
-    
-    with open(output_file, 'w') as outfile:
-        pass
-
-    cleaner = HTML_Cleaner()
-    rows = cleaner.split_on_tag(soup, "tr")
-    for row in rows:
-        row = row[1]
-        row = cleaner.replace_tag(row, "</td>", ',')
-        row = cleaner.replace_tag(row, "</th>", ',')
-        row = cleaner.remove(row, "<span class=\"visible-xs\">", "</span>")
-        row = cleaner.remove_tag(row, 'a')
-        row = cleaner.remove_tag(row, "span")
-        row = cleaner.remove_tag(row, "td")
-        row = cleaner.remove_tag(row, "th")
-        row = row[:-1]
-        row = row.split(",")
-
-        with open(output_file, 'a') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerow(row)
+    scraper = fdb_scraper()
+    scraper.scrape("https://www.footballdb.com/statistics/nfl/player-stats/passing/2022/regular-season?sort=passyds", "../data/passing.csv")
 
 if __name__ == "__main__":
     main()
